ee549fe72f240e1e3b0471546aa937500731b4b8
Albert Lombarte <albert@krakend.io>
2022-11-16 17:11:18 +0100

    Template for krakend check --audit flag

--- audit/main.go
+++ audit/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"encoding/json"
-	"os"
 	"strings"
 	"text/template"
+	"fmt"
+	"bytes"
+	"regexp"
+	"os"
 )
 
 func main() {
@@ -14,6 +17,7 @@ func main() {
 	t := template.New("test")
 	t = t.Funcs(template.FuncMap{
 		"add":       func(x, y int) int { return x + y },
+		"div":       func(x, y int) float64 { return float64(x) / float64(y) },
 		"hasPrefix": strings.HasPrefix,
 		"hasBit":    func(x float64, y int) bool { return (int(x) >> y) == 1 },
 	})
@@ -22,80 +26,119 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	err = t.Execute(os.Stdout, data)
+	var buf bytes.Buffer
+	err = t.Execute(&buf, data)
 	if err != nil {
 		panic(err)
 	}
+
+	// Remove tabs and extra new lines. Doing it in the template it's kind of hard to mantain
+	clean_template := regexp.MustCompile(`[\t\r\n]+`).ReplaceAllString(strings.TrimSpace(buf.String()), "\n")
+	fmt.Fprintf(os.Stdout,clean_template)
 }
 
 // Define a template.
 const tmpl = `
-{{- $ColorGreen := "\033[32;1m" -}}
-{{- $ColorWhite := "\033[37;1m" -}}
-{{- $ColorYellow := "\033[33;1m" -}}
-{{- $ColorRed := "\033[31;1m" -}}
-{{- $ColorBlue := "\033[34;1m" -}}
-{{- $ColorMagenta := "\033[36;1m" -}}
-{{- $ColorCyan := "\033[36;1m" -}}
-{{- $ColorReset := "\033[0m" -}}
-{{- $CRITICAL := printf "%s[CRITICAL]%s " $ColorMagenta $ColorReset -}}
-{{- $ERROR := printf "%s[ERROR]%s " $ColorRed $ColorReset -}}
-{{- $WARNING := printf "%s[WARNING]%s " $ColorYellow $ColorReset -}}
-{{- $INFO := printf "%s[INFO]%s " $ColorGreen $ColorReset -}}
-{{- $DEBUG := printf "%s[DEBUG]%s " $ColorCyan $ColorReset -}}
-{{- $nBackends := 0 -}}
-{{- $hasJWT := false -}}
-{{- $hasCB := false -}}
-{{- $hasRL := false -}}
-{{- $hasLogging := false -}}
-{{- $hasTele := false -}}
-{{- $nTele := 0 -}}
-{{- range $ks,$vs := .c -}}
-	{{- $hasRL = or $hasRL (eq $ks "qos/ratelimit/router") }}
-	{{- $hasLogging = or $hasLogging (eq $ks "telemetry/logging") }}
-	{{- $hasTele = or $hasTele (hasPrefix $ks "telemetry/") -}}
-	{{- if hasPrefix $ks "telemetry/"}}{{$nTele = add $nTele 1}}{{end}}
-{{- end -}}
-{{- range .e -}}
-    {{- $nBackends = add $nBackends ( len .b ) -}}
-    {{- range $ke,$ve := .c -}}
-        {{- $hasJWT = or $hasJWT (eq $ke "auth/validator") }}
-        {{- $hasRL = or $hasRL (eq $ke "qos/ratelimit/router") }}
-        {{- $hasRL = or $hasRL (eq $ke "qos/ratelimit/proxy") }}
-    {{- end}}
-    {{- range .b -}}
-        {{- range $kb,$vb := .c -}}
-			{{- $hasCB = or $hasCB (eq $kb "qos/circuit-breaker") }}
-			{{- $hasRL = or $hasRL (eq $kb "qos/ratelimit/proxy") }}
-        {{- end}}
-    {{- end}}
-{{- end}}
-{{- if len .e -}}
-    {{ $DEBUG}} There are {{ len .e }} endpoints configured
-{{ end -}}
-{{ if $nBackends -}}
-    {{ $DEBUG}} There are {{ $nBackends }} backends configured
-{{ end -}}
-{{$DEBUG}} You are{{ if not $hasJWT }} NOT{{end}} using JWT validation.
-{{ if not $hasCB -}}
-    {{ $WARNING}} Your backends are not protected with {{$ColorBlue}}qos/circuit-breaker{{$ColorReset}}.
-{{ end -}}
-{{ if not $hasRL -}}
-    {{ $WARNING}} You are exposing an All-You-Can-Eat API without any type of stateless rate limiting.
-{{ end -}}
-{{ if not $hasTele -}}
-    {{ $WARNING}} You don't have visibility of what is going on as you don't have any telemetry system enabled.
-{{ else -}}
-    {{ $DEBUG}} You have {{$nTele}} telemetry component(s) enabled.
-{{ end -}}
-{{ if not $hasLogging -}}
-    {{ $WARNING}} You don't have the {{$ColorBlue}}telemetry/logging{{$ColorReset}} component enabled, which is essential in any production installation.
-{{ end -}}
-{{ if not (ge (index .d 0) 32.0) -}}
+{{ $ColorGreen := "\033[32;1m" }}
+{{ $ColorWhite := "\033[37;1m" }}
+{{ $ColorYellow := "\033[33;1m" }}
+{{ $ColorRed := "\033[31;1m" }}
+{{ $ColorBlue := "\033[34;1m" }}
+{{ $ColorMagenta := "\033[36;1m" }}
+{{ $ColorCyan := "\033[36;1m" }}
+{{ $ColorReset := "\033[0m" }}
+{{ $CRITICAL := printf "%s[CRITICAL]%s " $ColorMagenta $ColorReset }}
+{{ $ERROR := printf "%s[ERROR]%s " $ColorRed $ColorReset }}
+{{ $WARNING := printf "%s[WARNING]%s " $ColorYellow $ColorReset }}
+{{ $INFO := printf "%s[INFO]%s " $ColorGreen $ColorReset }}
+{{ $DEBUG := printf "%s[DEBUG]%s " $ColorCyan $ColorReset }}
+{{ $nBackends := 0 }}
+{{ $hasJWT := false }}
+{{ $hasCB := false }}
+{{ $hasRL := false }}
+{{ $hasLogging := false }}
+{{ $hasAPIKeys := false }}
+{{ $hasTele := false }}
+{{ $nTele := 0 }}
+{{ $bitTLSEnabled := 5 }}
+{{ range $ks,$vs := .c }}
+	{{ $hasRL = or $hasRL (eq $ks "qos/ratelimit/router") }}
+	{{ $hasLogging = or $hasLogging (eq $ks "telemetry/logging") }}
+	{{ $hasTele = or $hasTele (hasPrefix $ks "telemetry/") }}
+	{{ if hasPrefix $ks "telemetry/"}}{{$nTele = add $nTele 1}}{{end}}
+{{ end }}
+{{ if .e }}
+{{ $nEndpoints := len .e }}
+	{{ range .e }}
+		{{ $nBackends = add $nBackends ( len .b ) }}
+		{{ range $ke,$ve := .c }}
+			{{ $hasJWT = or $hasJWT (eq $ke "auth/validator") }}
+			{{ $hasRL = or $hasRL (eq $ke "qos/ratelimit/router") }}
+			{{ $hasRL = or $hasRL (eq $ke "qos/ratelimit/proxy") }}
+			{{ $hasAPIKeys = or $hasAPIKeys (eq $ke "auth/api-keys") }}
+		{{ end }}
+		{{ range .b }}
+			{{ range $kb,$vb := .c }}
+				{{ $hasCB = or $hasCB (eq $kb "qos/circuit-breaker") }}
+				{{ $hasRL = or $hasRL (eq $kb "qos/ratelimit/proxy") }}
+			{{ end }}
+		{{ end }}
+	{{ end }}
+
+
+	{{/* START TEMPLATE */}}
+
+	{{ $DEBUG }} There are {{ $nEndpoints }} endpoints configured
+
+	{{ if $nBackends }}
+		{{ $DEBUG }} There are {{ $nBackends }} backends configured
+	{{ end }}
+
+	{{ if and $nBackends $nEndpoints }}
+		{{ $avg := div $nBackends $nEndpoints }}
+		{{ if lt $avg 1.3}}
+		{{$WARNING}} You are not taking advantage of aggregation. There are only {{ printf "%.2f" $avg }} backends per endpoint.
+		{{else}}
+		{{$DEBUG}} There are {{ printf "%.2f" $avg }} backends per endpoint
+		{{end}}
+	{{ end }}
+
+	{{ if $hasJWT }}
+		{{$DEBUG}} You have endpoints configured with JWT validation.
+	{{ else }}
+		{{$WARNING}} No endpoint is protected by JWT
+	{{ end }}
+
+	{{ if $hasAPIKeys }}
+		{{$DEBUG}} You have endpoints requiring API Keys.
+	{{ end }}
+
+
+	{{ if not $hasCB }}
+		{{ $WARNING}} Your backends are not protected with {{$ColorBlue}}qos/circuit-breaker{{$ColorReset}}.
+	{{ end }}
+
+	{{ if not $hasRL }}
+		{{ $WARNING}} You are exposing an All-You-Can-Eat API without any type of stateless rate limiting.
+	{{ end }}
+
+{{ else }}
+	{{ $ERROR }} No endpoints defined!
+{{ end}}
+{{/* SERVICE SETTINGS */}}
+{{ if not $hasTele }}
+	{{ $WARNING}} Hope you are good reading logs, because you don't have any telemetry system enabled.
+{{ else }}
+	{{ $DEBUG}} You have {{$nTele}} telemetry component(s) enabled.
+{{ end }}
+{{ if not $hasLogging }}
+	{{ $WARNING}} You don't have the {{$ColorBlue}}telemetry/logging{{$ColorReset}} component enabled, which is essential in any production installation.
+{{ end }}
+{{ if not (ge (index .d 0) 32.0) }}
 	{{$INFO}} You are not using TLS. Hopefully you are terminating SSL before KrakenD.
-{{ else if not (hasBit (index .d 0) 5) -}}
+{{ else if not (hasBit (index .d 0) $bitTLSEnabled) }}
 	{{$WARNING}} You have configured TLS but it's disabled.
-{{ end -}}
+{{ end }}
 `
 
 const source = `{
@@ -119,7 +162,9 @@ const source = `{
 					"c": {}
 				}
 			],
-			"c": {}
+			"c": {
+				"auth/api-keys": []
+			}
 		},
 		{
 			"d": [
@@ -771,42 +816,5 @@ const source = `{
 		"security/cors": [],
 		"telemetry/logging": [],
 		"telemetry/metrics": []
-	},
-
-    "auth_validation": {
-        "cache": true
-    },
-    "qos_circuit_breaker": true,
-    "proxy_sequential_total": 3,
-    "stats_noop_percentage": 75,
-    "stats_endpoints_total": 95,
-    "stats_backends_total": 3,
-    "stats_async_total": 95,
-    "stats_aggregation_average": 1.2,
-    "tls": false,
-    "security_http": false,
-    "ratelimit": false,
-    "stats_telemetry_systems": 1,
-    "telemetry_logging": {
-        "enabled": true
-    },
-    "name": false,
-    "wildcard_query_strings": true,
-    "wildcard_headers": true,
-
-    "router": {
-        "disable_access_log": true
-    },
-
-
-    "timeouts excesius": true,
-    "connectivity? (ws, graphql, lambda, pubsub, etc)": true,
-    "nombre d’endpoints protegits amb JWT": true,
-    "altres sistemes d’autenticació/autorització": true,
-    "custom plugins injectats": true,
-    "krakend version als headers de resposta": true,
-    "cors, jose, etc: flags de debug activats": true,
-    "excessives operacions de flatmap": true,
-    "excessives expr de cel": true,
-    "flatmap + basic manipulation": true
+	}
 }`
